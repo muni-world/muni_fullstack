fd01f8117bb6aefdfdf94aa8e1302d65cc2084b4
quokka <devonsaliga@gmail.com>
2025-02-06 07:41:47 -0500

    github actions config troubleshooting 3

--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -1,29 +1,32 @@
 /**
  * Import function triggers from their respective submodules:
  *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import * as functions from "firebase-functions";
-import * as logger from "firebase-functions/logger";
-
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+import {onRequest} from "firebase-functions/v2/https";
+import {onDocumentCreated, DocumentSnapshot} from "firebase-functions/v2/firestore";
 
 // Example HTTP function
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
+export const helloWorld = onRequest((request, response) => {
+  console.info("Hello logs!", {structuredData: true});
   response.json({message: "Hello from Firebase!"});
 });
 
+// Define the interface for your user data structure
+interface UserData {
+  // Add your user properties here, for example:
+  name?: string;
+  email?: string;
+  createdAt?: string;
+}
+
 // Example Firestore trigger function
-export const onUserCreated = functions.firestore
-  .document("users/{userId}")
-  .onCreate((snap, context) => {
-    const newUser = snap.data();
-    functions.logger.info("New user created:", newUser);
+export const onUserCreated = onDocumentCreated("users/{userId}", 
+  (event) => {
+    const snapshot = event.data as DocumentSnapshot<UserData>;
+    const userData = snapshot.data();
+    console.info("New user created:", userData);
     return null;
-  });
+  }
+);
